6af93360e2ee0b55c364ea28ae16110653949c46
Kuvarti <uzali.kafasi@gmail.com>
2023-05-23 23:25:23 +0300

    add create db and user

--- db/createTables.sql
+++ db/createTables.sql
@@ -1,9 +1,19 @@
+drop database if exists transcendence;
+drop user if exists client;
+
+create database transcendence;
+create user client with PASSWORD '123';
+GRANT ALL PRIVILEGES ON DATABASE transcendence TO client;
+ALTER ROLE client CREATEDB;
+USE transcendence;
+
 drop table if exists matches;
 drop table if exists friendlist;
 drop table if exists users;
 drop type if exists winner;
 drop function if exists addmatch(p1 int, p2 int, p1_s int, p2_s int);
 
+-- users
 create table users(
 	id			serial unique not null primary key,
 	username	varchar(16) unique not null check(length(username) > 2),
@@ -12,6 +22,7 @@ create table users(
 	-- ....
 );
 
+-- played matches
 create table matches(
 	match_id	serial unique not null primary key,
 	player1		integer not null references users(id),
@@ -21,12 +32,14 @@ create table matches(
 	m_winner	integer check(m_winner = player1 or m_winner = player2 or m_winner = 0)
 );
 
+-- users friends lists
 create table friendlist(
 	user1		integer not null references users(id),
 	user2		integer not null references users(id),
 	primary key(user1, user2)
 );
 
+-- Adding Match Results
 create or replace function addmatch(p1 int, p2 int, p1_s int, p2_s int)
 	returns void
 as $$
