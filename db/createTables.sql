84a087239fe90ab6a2aa95a6f89ee359a163b7dc
Kuvarti <uzali.kafasi@gmail.com>
2023-05-24 11:07:49 +0300

    adding test values

--- db/createTables.sql
+++ db/createTables.sql
@@ -1,3 +1,4 @@
+/*
 drop database if exists transcendence;
 drop user if exists client;
 
@@ -6,6 +7,7 @@ create user client with PASSWORD '123';
 GRANT ALL PRIVILEGES ON DATABASE transcendence TO client;
 ALTER ROLE client CREATEDB;
 USE transcendence;
+--*/
 
 drop table if exists matches;
 drop table if exists friendlist;
@@ -13,7 +15,6 @@ drop table if exists users;
 drop type if exists winner;
 drop function if exists addmatch(p1 int, p2 int, p1_s int, p2_s int);
 
--- users
 create table users(
 	id			serial unique not null primary key,
 	username	varchar(16) unique not null check(length(username) > 2),
@@ -22,7 +23,6 @@ create table users(
 	-- ....
 );
 
--- played matches
 create table matches(
 	match_id	serial unique not null primary key,
 	player1		integer not null references users(id),
@@ -32,14 +32,12 @@ create table matches(
 	m_winner	integer check(m_winner = player1 or m_winner = player2 or m_winner = 0)
 );
 
--- users friends lists
 create table friendlist(
 	user1		integer not null references users(id),
 	user2		integer not null references users(id),
 	primary key(user1, user2)
 );
 
--- Adding Match Results
 create or replace function addmatch(p1 int, p2 int, p1_s int, p2_s int)
 	returns void
 as $$
@@ -53,3 +51,31 @@ begin
 	end if;
 end;
 $$ language plpgsql ;
+
+--  /* Test datas!
+insert into users (username, rank, passwd) values
+	('kuvarti', 1000, 'hashedpasswd'),
+	('pro', 999, 'hashedpasswd'),
+	('normal player', 500, 'hashedpasswd'),
+	('noob', 10, 'hashedpasswd');
+
+select addmatch(1, 2, 5, 3);
+select addmatch(3, 4, 2, 1);
+
+select addmatch(2, 3, 3, 5);
+select addmatch(4, 1, 1, 2);
+
+select addmatch(1, 3, 4, 4);
+select addmatch(2, 4, 2, 2);
+
+--!FAIL:	This hould not happen!
+insert into friendlist (user1, user2) values
+	(1, 2),
+	(2, 1);
+
+select * from users;
+select * from matches;
+select u1.username as player1, u2.username as player2, matches.m_winner
+from matches, users as u1, users as u2
+where matches.player1 = u1.id and matches.player2 = u2.id;
+--*/
